22f9985a9eaa44f3abdf9bb5171104e691aa74e6
Revanth Venkateswar <revanth.jsr@gmail.com>
2024-06-07 10:15:36 +0530

    Completed backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
 
-app.get("/todos", function (req, res) {});
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find({});
 
-app.post("/todo", function (req, res) {
+  res.json({
+    todos,
+  });
+});
+
+app.post("/todo", async function (req, res) {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
@@ -15,10 +22,20 @@ app.post("/todo", function (req, res) {
     });
     return;
   }
+
   // put it in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  res.json({
+    msg: "Todo created",
+  });
 });
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
@@ -27,5 +44,20 @@ app.put("/completed", function (req, res) {
     });
     return;
   }
+
   // put it in mongodb
+  await todo.update(
+    {
+      _id: req.body._id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
+
+app.listen(3000);
