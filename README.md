27e0e0e08edc417c6e13e04b815ab0e19c6f42dd
Revanth Venkateswar <revanth.jsr@gmail.com>
2024-06-07 10:51:27 +0530

    Fixed db.js | get, post endpoints working, put is giving an error

--- README.md
+++ README.md
@@ -6,6 +6,3 @@ It has the following features -
 - Anyone can create a todo
 - Anyone can see their existing todos
 - Anyone can mark a todos as done
-
-// initialize a node project
-// put a package.json
--- backend/db.js
+++ backend/db.js
@@ -6,13 +6,14 @@
  }
  */
 const mongoose = require("mongoose");
+const { boolean } = require("zod");
 mongoose.connect(
   "mongodb+srv://revanth:mongoDB1021@cluster0.ddhmf2m.mongodb.net/todos"
 );
 const todoSchema = mongoose.Schema({
-  title: string,
-  description: string,
-  completed: boolean,
+  title: String,
+  description: String,
+  completed: Boolean,
 });
 
 const todo = mongoose.model("todos", todoSchema);
